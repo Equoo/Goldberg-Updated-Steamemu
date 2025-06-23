027d5597ce4e3093ab61f6e6410f4bc092c50473
Equo <david.derny@ik.me>
2025-06-23 04:14:36 +0200

    Refactor build scripts: separate build steps for steam_api, experimental, and lobby connect

--- build_win_find_interfaces.bat
+++ build_win_find_interfaces.bat
@@ -1,24 +1,9 @@
-
+@echo off
 cd /d "%~dp0"
 mkdir release\tools
 del /Q release\tools\*
-
 call build_env_x86.bat
-
-:: Create the response file
-(
-    echo generate_interfaces_file.cpp
-    echo /EHsc
-    echo /MP12
-    echo /Ox
-    echo /link
-    echo /debug:none
-    echo /OUT:release\tools\generate_interfaces_file.exe
-) > compile_args.rsp
-
-:: Compile using the response file
-cl @compile_args.rsp
-
+cl generate_interfaces_file.cpp /EHsc /MP12 /Ox /link /debug:none /OUT:release\tools\generate_interfaces_file.exe
 del /Q /S release\tools\*.lib
 del /Q /S release\tools\*.exp
 copy Readme_generate_interfaces.txt release\tools\Readme_generate_interfaces.txt
